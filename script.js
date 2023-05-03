1aeec3bb4f64203ad4ee9c68919b7052aeecc2ff
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-05-03 16:33:10 -0700

    Fixed issues with row and col dimensions

--- script.js
+++ script.js
@@ -84,8 +84,8 @@ function init(cell) {
             let mineCount = 0;
             var cellRow = cell.parentNode.rowIndex;
             var cellCol = cell.cellIndex;
-            for (let r = Math.max(cellRow - 1, 0); r <= Math.min(cellRow + 1, 9); r++) {
-                for (let c = Math.max(cellCol - 1, 0); c <= Math.min(cellCol + 1, 9); c++) {
+            for (let r = Math.max(cellRow - 1, 0); r <= Math.min(cellRow + 1, rowDimensions - 1); r++) {
+                for (let c = Math.max(cellCol - 1, 0); c <= Math.min(cellCol + 1, colDimensions - 1); c++) {
                     if (grid.rows[r].cells[c].getAttribute('mine') == 'true') {
                         mineCount++;
                     }
@@ -94,8 +94,8 @@ function init(cell) {
             cell.innerHTML = mineCount;
             if (mineCount == 0) {
                 // If the cell doesn't have a mine
-                for (let r = Math.max(cellRow - 1, 0); r <= Math.min(cellRow + 1, 9); r++) {
-                    for (let c = Math.max(cellCol - 1, 0); c <= Math.min(cellCol + 1, 9); c++) {
+                for (let r = Math.max(cellRow - 1, 0); r <= Math.min(cellRow + 1, rowDimensions - 1); r++) {
+                    for (let c = Math.max(cellCol - 1, 0); c <= Math.min(cellCol + 1, colDimensions - 1); c++) {
                         if (grid.rows[r].cells[c].innerHTML == '') {
                             init(grid.rows[r].cells[c]);
                         }
